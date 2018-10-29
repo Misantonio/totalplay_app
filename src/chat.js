401f0c3a1b7614bd3dd8ce5baea0ffc59296d5f5
Misael Alarcón <malarcon@nearshoremx.com>
2018-10-29 10:58:34 -0600

    Display marker info

--- src/chat.js
+++ src/chat.js
@@ -68,7 +68,7 @@ export default class Chat extends Component {
                     },
                 },
             ],
-        })
+        });
     }
 
     onSend(messages = []) {
--- src/maps.js
+++ src/maps.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Text, Image } from 'react-native';
 import MapView, { PROVIDER_DEFAULT, Marker } from 'react-native-maps';
 
 
 const coords = [[19.357633, -99.273455], [19.361445, -99.278597], [19.365305, -99.264662], [19.417342, -99.148454]];
+const info = [
+    'Zona: Santa Fe. \n Hub No. 2031',
+    'Zona: Santa Fe. \n Hub No. 2231',
+    'Zona: Santa Fe. \n Hub No. 2041',
+    'Zona: Doctores. \n Hub No. 3011'
+];
 const width = Dimensions.get('window').width;
 
 export default class MyMap extends Component {
@@ -13,7 +19,9 @@ export default class MyMap extends Component {
             mapRegion: null,
             lastRegion: null,
             delta: null,
-            markers: []
+            markers: [],
+            markerPressed: null,
+            pressed: false
         }
     }
 
@@ -38,29 +46,65 @@ export default class MyMap extends Component {
 
     onRegionChange(region) {
         this.setState({
-            region: region
+            region: region,
+            moving: true
         });
     }
 
-    onRegionCompleteChange(){
+    onPressMarker = (args) => {
+        let id = args.nativeEvent.id;
+        let pressed = id != this.state.markerPressed ? true : false;
+        this.setState({
+            pressed: pressed,
+            markerPressed: id
+        });
+        this.setMarkers(!this.state.pressed, id);
+    }
+
+    setMarkers(pressed, markerPressed){
         let markers = [];
         let delta = this.state.region.latitudeDelta;
         if(delta <= 0.05){
             for (let i = 0; i < coords.length; i++) {
-                markers.push(
-                    <Marker
-                        key={i}
-                        coordinate={{
-                            latitude: coords[i][0],
-                            longitude: coords[i][1],
-                        }}>
-                    </Marker>
-                )
+                if (markerPressed == i && pressed){
+                    markers.push(
+                        <Marker
+                            tracksViewChanges={false}
+                            identifier={String(i)}
+                            onPress={this.onPressMarker}
+                            key={Math.round(Math.random()*10000)}
+                            coordinate={{
+                                latitude: coords[i][0],
+                                longitude: coords[i][1],
+                            }}>
+                            <View style={styles.rectangle}>
+                                <Text style={styles.mapsInfo}>{info[markerPressed]}</Text>
+                            </View>
+                            <View style={styles.triangle}></View>
+                        </Marker>
+                    )
+                } else {
+                    markers.push(
+                        <Marker
+                            identifier={String(i)}
+                            onPress={this.onPressMarker}
+                            key={Math.round(Math.random()*10000)}
+                            coordinate={{
+                                latitude: coords[i][0],
+                                longitude: coords[i][1],
+                            }}>
+                        </Marker>
+                    )
+                }
             }
         }
         this.setState({
             markers: markers
-        });
+        })
+    }
+
+    onRegionCompleteChange(){
+        this.setMarkers(this.state.pressed, this.state.markerPressed);
     }
 
     render() {
@@ -98,5 +142,36 @@ const styles = StyleSheet.create({
         left: 0,
         bottom: 0,
         right: 0
+    },
+    rectangle: {
+        width: 100,
+        height: 35,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderColor: 'red',
+        borderWidth: 2,
+        alignContent: 'center'
+    },
+    triangle: {
+        width: 0,
+        height: 0,
+        backgroundColor: 'transparent',
+        borderStyle: 'solid',
+        borderLeftWidth: 50,
+        borderRightWidth: 50,
+        borderBottomWidth: 20,
+        borderLeftColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderBottomColor: 'red',
+        transform: [{
+            rotate: '180deg'
+        }]
+    },
+    mapsInfo: {
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 10,
+        marginBottom: 5,
     }
 });
